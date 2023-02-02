47d79e944a4797294e8964db79b9daeac8dd4aad
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-03 01:46:34 +0530

    Updated View, Controller, And Fragment

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,22 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-13_Fragment.controller.View1", {
+		
+		onInit : function(){
+			
+			var oData = new sap.ui.model.json.JSONModel();
+			oData.loadData("model/fruits.json");
+			this.getView().setModel(oData);
+		},
+		
+		onOpenFrag : function(){
+			this.frag = new sap.ui.xmlfragment("GithubSAP-UI5-Fiori-13_Fragment.fragments.fruits",this);
+			this.getView().addDependent(this.frag);
+			this.frag.open();
+		},
+		onDialogClose : function (){
+			this.frag.close();
+		}
 
 	});
 });
